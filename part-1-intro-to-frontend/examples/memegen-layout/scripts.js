21a9cc54c03d4f36095c575bbff54b9573fe770d
Falafel72 <kylehu2000@gmail.com>
2019-08-31 16:54:56 -0700

    updated memegen-layout css to match the product

---
+++ part-1-intro-to-frontend/examples/memegen-layout/scripts.js
@@ -0,0 +1,32 @@
+let memes = [
+  {
+    name: "Distracted Boyfriend",
+    url: "https://i.imgflip.com/1ur9b0.jpg",
+    width: 1200,
+    height: 800
+  },
+  {
+    name: "Drake Hotline Bling",
+    url: "https://i.imgflip.com/30b1gx.jpg",
+    width: 1200,
+    height: 1200
+  },
+  {
+    name: "Two Buttons",
+    url: "https://i.imgflip.com/1g8my4.jpg",
+    width: 600,
+    height: 908
+  },
+  {
+    name: "Mocking Spongebob",
+    url: "https://i.imgflip.com/1otk96.jpg",
+    width: 502,
+    height: 353
+  },
+  {
+    name: "Change My Mind",
+    url: "https://i.imgflip.com/24y43o.jpg".
+    width: 482,
+    height: 361
+  }
+];
